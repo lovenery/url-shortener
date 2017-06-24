08f1bf5e7cd09b48b9b01d8b0f3a4e7e83cebb54
Hsu <edittye379@gmail.com>
2017-06-24 23:13:11 +0800

    override save(), my utils

---
+++ src/shortener/utils.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def create_shortcode(instance, size=6):
+    new_code = code_generator(size=size)
+    # print(instance, instance.__class__, instance.__class__.__name__)
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(shortcode=new_code).exists()
+    if qs_exists:
+        return create_shortcode(size=size)
+    return new_code
