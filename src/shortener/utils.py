1f66fc4a826aa8046c707aaf7ac0d7a59c12a4a7
Hsu <edittye379@gmail.com>
2017-06-25 14:28:14 +0800

    settings parameters

--- src/shortener/utils.py
+++ src/shortener/utils.py
@@ -1,10 +1,13 @@
 import random
 import string
+from django.conf import settings
+
+SHORTCODE_MIN = getattr(settings, "SHORTCODE_MIN", 6)
 
 def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-def create_shortcode(instance, size=6):
+def create_shortcode(instance, size=SHORTCODE_MIN):
     new_code = code_generator(size=size)
     # print(instance, instance.__class__, instance.__class__.__name__)
     Klass = instance.__class__
