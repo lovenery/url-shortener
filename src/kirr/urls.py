d63da6ded9e6a7b2d2607982eee297647ec96817
Hsu <edittye379@gmail.com>
2017-06-26 12:41:50 +0800

    views

--- src/kirr/urls.py
+++ src/kirr/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from shortener.views import kirr_redirect_view, KirrRedirectView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^(?P<shortcode>[\w-]{6,15})/$', kirr_redirect_view),
+    url(r'^b/(?P<shortcode>[\w-]{6,15})/$', KirrRedirectView.as_view()),
 ]
--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views import View
 
-# Create your views here.
+from .models import KirrURL
+
+def kirr_redirect_view(request, shortcode=None, *args, **kwargs): # function based view FBV
+    obj = get_object_or_404(KirrURL, shortcode=shortcode)
+    # return HttpResponse("HI {sc}".format(sc=obj.url))
+    return HttpResponseRedirect(obj.url)
+
+class KirrRedirectView(View): # class based view CBV
+    def get(self, request, shortcode=None, *args, **kwargs):
+        obj = get_object_or_404(KirrURL, shortcode=shortcode)
+        return HttpResponse("HI again {sc}".format(sc=obj.url))
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse()
