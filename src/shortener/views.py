db4aa804c8e7775683784375e637c01ff6a2b167
Hsu <edittye379@gmail.com>
2017-06-26 23:57:34 +0800

    remove unused & add home page

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -4,15 +4,18 @@ from django.views import View
 
 from .models import KirrURL
 
-def kirr_redirect_view(request, shortcode=None, *args, **kwargs): # function based view FBV
-    obj = get_object_or_404(KirrURL, shortcode=shortcode)
-    # return HttpResponse("HI {sc}".format(sc=obj.url))
-    return HttpResponseRedirect(obj.url)
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "shortener/home.html", {})
+
+    def post(self, request, *args, **kwargs):
+        return render(request, "shortener/home.html", {})
 
 class KirrRedirectView(View): # class based view CBV
     def get(self, request, shortcode=None, *args, **kwargs):
         obj = get_object_or_404(KirrURL, shortcode=shortcode)
-        return HttpResponse("HI again {sc}".format(sc=obj.url))
+        # return HttpResponse("URL: {sc}".format(sc=obj.url))
+        return HttpResponseRedirect(obj.url)
 
     def post(self, request, *args, **kwargs):
         return HttpResponse()
