4a0153e10470ff5b1547e33381d5c809b390d202
Hsu <edittye379@gmail.com>
2017-06-27 20:34:02 +0800

    done

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -16,13 +16,25 @@ class HomeView(View):
 
     def post(self, request, *args, **kwargs):
         form = SubmitUrlForm(request.POST)
-        if form.is_valid():
-            print("views.py, if form.is_valid(), form.cleaned_data: ", form.cleaned_data)
         context = {
-            "title": "Result",
+            "title": "Whoops!",
             "form": form
         }
-        return render(request, "shortener/home.html", context)
+        template = "shortener/home.html"
+        if form.is_valid():
+            # print("views.py, if form.is_valid(), form.cleaned_data: ", form.cleaned_data)
+            new_url = form.cleaned_data.get("url")
+            obj, created = KirrURL.objects.get_or_create(url=new_url)
+            context = {
+                "object": obj,
+                "created": created,
+            }
+            if created:
+                template = "shortener/success.html"
+            else:
+                template = "shortener/already-exists.html"
+
+        return render(request, template, context)
 
 class KirrRedirectView(View): # class based view CBV
     def get(self, request, shortcode=None, *args, **kwargs):
